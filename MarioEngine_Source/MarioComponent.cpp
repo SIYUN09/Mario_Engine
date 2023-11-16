331b963bbb9c9375be90049dbc0b26fc05145116
김시윤 <znznl@naver.com>
2023-11-16 18:12:07 +0900

    Component

---
+++ MarioEngine_Source/MarioComponent.cpp
@@ -0,0 +1,31 @@
+#include "MarioComponent.h"
+
+namespace Mario
+{
+	Component::Component()
+	{
+	}
+	Component::~Component()
+	{
+	}
+
+	void Component::Initialize()
+	{
+
+	}
+
+	void Component::Update()
+	{
+
+	}
+
+	void Component::LateUpdate()
+	{
+
+	}
+
+	void Component::Render(HDC hdc)
+	{
+
+	}
+}
---
+++ MarioEngine_Source/MarioTransform.cpp
@@ -0,0 +1,23 @@
+#include "MarioTransform.h"
+
+namespace Mario
+{
+	Transform::Transform()
+	{
+	}
+	Transform::~Transform()
+	{
+	}
+	void Transform::Initialize()
+	{
+	}
+	void Transform::Update()
+	{
+	}
+	void Transform::LateUpdate()
+	{
+	}
+	void Transform::Render(HDC hdc)
+	{
+	}
+}
